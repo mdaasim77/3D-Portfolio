879e84dfd8603bff19c5ce166ce74cfacdcfc0ac
mdaasim77 <khanaasim9910@gmail.com>
2026-02-16 15:31:39 +0530

    add dependency and change app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { Canvas } from "@react-three/fiber";
 
 export default function App() {
-  return <div>3D Portfolio</div>;
+  return (
+    <Canvas camera={{ position: [0, 2, 8], fov: 50 }}>
+      <ambientLight intensity={1} />
+      <mesh>
+        <boxGeometry />
+        <meshStandardMaterial color="orange" />
+      </mesh>
+    </Canvas>
+  );
 }
