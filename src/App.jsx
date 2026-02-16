f33c041d7c65b6d2ece7b6c882bcadc0c610845c
mdaasim77 <khanaasim9910@gmail.com>
2026-02-16 17:38:30 +0530

    working with intensity light direction and more

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import React from "react";
 import { Canvas } from "@react-three/fiber";
 
 export default function App() {
   return (
-    <Canvas camera={{ position: [0, 2, 8], fov: 50 }}>
+    <Canvas camera={{ position: [2, 2, 5], fov: 30 }}>
       <ambientLight intensity={1} />
+      <directionalLight position={[2, 4, 6]} intensity={1.5} castShadow />
       <mesh>
         <boxGeometry />
         <meshStandardMaterial color="orange" />
