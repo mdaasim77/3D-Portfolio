7686b9a022a7162841bebf4fb87e170d4ca9fb85
mdaasim77 <khanaasim9910@gmail.com>
2026-02-17 15:00:15 +0530

    black screen apply

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,38 @@
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Environment, Stars } from "@react-three/drei";
 import Laptop from "./Components/Laptop";
+import StartScreen from "./Ui/StartScreen";
 
 export default function App() {
   return (
-    <Canvas camera={{ position: [0, 1.8, 8], fov: 5 }}>
-      <ambientLight intensity={0.3} />
-      <directionalLight position={[5, 10, 5]} intensity={1.5} />
+    <>
+      <StartScreen />
+      <Canvas camera={{ position: [0, 1.8, 8], fov: 5 }}>
+        <ambientLight intensity={0.3} />
+        <directionalLight position={[5, 10, 5]} intensity={1.5} />
 
-      {/* Moving star particles = depth */}
-      <Stars
-        radius={80}
-        depth={50}
-        count={5000}
-        factor={4}
-        saturation={0}
-        fade
-        speed={1}
-      />
+        {/* Moving star particles = depth */}
+        <Stars
+          radius={80}
+          depth={50}
+          count={5000}
+          factor={4}
+          saturation={0}
+          fade
+          speed={1}
+        />
 
-      <Environment
-        files="/src/assets/hdr/space.hdr"
-        background
-        backgroundIntensity={0.3}
-        backgroundBlurriness={0.3}
-      />
+        <Environment
+          files="/src/assets/hdr/space.hdr"
+          background
+          backgroundIntensity={0.3}
+          backgroundBlurriness={0.3}
+        />
 
-      <Laptop scale={0.008} />
+        <Laptop scale={0.008} />
 
-      <OrbitControls enablePan={false} minDistance={5} maxDistance={25} />
-    </Canvas>
+        <OrbitControls enablePan={false} minDistance={5} maxDistance={25} />
+      </Canvas>
+    </>
   );
 }
---
+++ src/Ui/StartScreen.jsx
@@ -0,0 +1,9 @@
+export default function StartScreen() {
+  return (
+    <div className="startScreen">
+      <h1>My 3D Portfolio</h1>
+      <p>Creative Developer Experience</p>
+      <button>Start Experience</button>
+    </div>
+  );
+}
