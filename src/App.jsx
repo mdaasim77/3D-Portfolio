102ffdabed2feb5c2fb99b4bd50556e878117658
mdaasim77 <khanaasim9910@gmail.com>
2026-02-17 12:01:03 +0530

    3d icon on main screen

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Laptop from "./Components/Laptop";
 
 export default function App() {
   return (
-    <Canvas camera={{ position: [10, 3, 30], fov: 1 }}>
+    <Canvas camera={{ position: [0, 3, 30], fov: 1 }}>
       <ambientLight intensity={1} />
       <directionalLight position={[5, 10, 5]} intensity={2} />
 
