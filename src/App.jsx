abab77ea6e22d1c8e946a611f167f4f978f63e28
mdaasim77 <khanaasim9910@gmail.com>
2026-02-17 14:24:21 +0530

    working with background

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, Environment, Stars } from "@react-three/drei";
 import Laptop from "./Components/Laptop";
 
 export default function App() {
   return (
-    <Canvas camera={{ position: [0, 3, 30], fov: 1 }}>
-      <ambientLight intensity={1} />
-      <directionalLight position={[5, 10, 5]} intensity={2} />
+    <Canvas camera={{ position: [0, 1.8, 8], fov: 5 }}>
+      <ambientLight intensity={0.3} />
+      <directionalLight position={[5, 10, 5]} intensity={1.5} />
 
-      <Laptop scale={0.01} />
+      {/* Moving star particles = depth */}
+      <Stars
+        radius={80}
+        depth={50}
+        count={5000}
+        factor={4}
+        saturation={0}
+        fade
+        speed={1}
+      />
 
-      <OrbitControls />
+      <Environment
+        files="/src/assets/hdr/space.hdr"
+        background
+        backgroundIntensity={0.3}
+        backgroundBlurriness={0.3}
+      />
+
+      <Laptop scale={0.008} />
+
+      <OrbitControls enablePan={false} minDistance={5} maxDistance={25} />
     </Canvas>
   );
 }
