2ccb3362e0427e3cb376fa058df6dcdde6986144
mdaasim77 <khanaasim9910@gmail.com>
2026-02-20 14:22:23 +0530

    remove extra code

--- src/Components/CameraController.jsx
+++ src/Components/CameraController.jsx
@@ -1,38 +1,3 @@
-// import { useThree } from "@react-three/fiber";
-// import { useLayoutEffect, useEffect } from "react";
-// import gsap from "gsap";
-
-// export default function CameraController({ start, onComplete }) {
-//   const { camera } = useThree();
-
-//   const lookX = 52;
-//   const lookY = 2;
-//   const lookZ = 0;
-
-//   // Initial far shot
-//   useLayoutEffect(() => {
-//     camera.position.set(10, 5, 80);
-//     camera.lookAt(lookX, lookY, lookZ);
-//   }, []);
-
-//   useEffect(() => {
-//     if (!start) return;
-
-//     gsap.to(camera.position, {
-//       x: 50,
-//       y: 2,
-//       z: 4,
-//       duration: 6,
-//       ease: "power3.inOut",
-//       onUpdate: () => camera.lookAt(lookX, lookY, lookZ),
-//       onComplete: onComplete, // 🔥 triggers Hero
-//     });
-
-//   }, [start]);
-
-//   return null;
-// }
-
 
 import { useThree } from "@react-three/fiber";
 import { useLayoutEffect, useEffect } from "react";
--- src/Ui/Hero.jsx
+++ src/Ui/Hero.jsx
@@ -1,35 +1,3 @@
-// import { useEffect, useRef } from "react";
-// import gsap from "gsap";
-
-// export default function Hero() {
-//   const ref = useRef();
-
-//   useEffect(() => {
-//     gsap.fromTo(
-//       ref.current,
-//       { y: 60, opacity: 0 },
-//       {
-//         y: 0,
-//         opacity: 1,
-//         duration: 1.6,
-//         ease: "power3.out",
-//       },
-//     );
-//   }, []);
-
-//   return (
-//     <section className="hero">
-//       <div ref={ref} className="heroLeft">
-//         <h1>Smart AI Workspace</h1>
-//         <p>
-//           Experience productivity in a new dimension. Manage tasks and build
-//           faster.
-//         </p>
-//         <button className="heroBtn">Explore Product</button>
-//       </div>
-//     </section>
-//   );
-// }
 
 
 import { useEffect, useRef } from "react";
