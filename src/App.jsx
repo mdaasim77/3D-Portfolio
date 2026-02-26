75f22423925836a8d831424285f001510e768936
mdaasim77 <khanaasim9910@gmail.com>
2026-02-26 11:17:30 +0530

    remove extra code

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,3 @@
-// import { Canvas } from "@react-three/fiber";
-// import { Environment, Stars, OrbitControls } from "@react-three/drei";
-// import Laptop from "./Components/Laptop";
-// import StartScreen from "./Ui/StartScreen";
-// import { useState } from "react";
-// import CameraController from "./Components/CameraController";
-// import Hero from "./Ui/Hero";
-
-// export default function App() {
-//   const [start, setStart] = useState(false);
-//   const [cameraDone, setCameraDone] = useState(false);
-//   const [explore, setExplore] = useState(false);
-//   const [focusLaptop, setFocusLaptop] = useState(false);
-
-//   return (
-//     <>
-//       {!start && <StartScreen onStart={() => setStart(true)} />}
-//       {/* {cameraDone && <Hero onExplore={() => setExplore(true)} />} */}
-//       {start && !explore && !focusLaptop && (
-//         <Hero onExplore={() => setExplore(true)} />
-//       )}
-//       <Canvas camera={{ position: [50, 36, 45], fov: 40 }}>
-//         <CameraController
-//           start={start}
-//           explore={explore}
-//           focusLaptop={focusLaptop}
-//           onComplete={() => setCameraDone(true)}
-//         />
-
-//         <ambientLight intensity={0.3} />
-//         <directionalLight position={[20, 10, 20]} intensity={1.5} />
-
-//         <Stars
-//           radius={80}
-//           depth={50}
-//           count={100000}
-//           factor={4}
-//           saturation={0}
-//           fade
-//           speed={1}
-//         />
-
-//         <Environment
-//           files="/src/assets/hdr/space.hdr"
-//           background
-//           backgroundIntensity={0.1}
-//           backgroundBlurriness={0.1}
-//         />
-
-//         {/* <Laptop onLaptopClick={() => setFocusLaptop(true)} /> */}
-//         <Laptop
-//           focusLaptop={focusLaptop}
-//           onLaptopClick={() => setFocusLaptop(true)}
-//         />
-//         {/* enable mouse control only after explore */}
-
-//         {explore && !focusLaptop && (
-//           <OrbitControls
-//             target={[52, 2, 0]}
-//             enablePan={false}
-//             minDistance={5}
-//             maxDistance={120}
-//           />
-//         )}
-//       </Canvas>
-//     </>
-//   );
-// }
-
 import { Canvas } from "@react-three/fiber";
 import { Environment, Stars, OrbitControls } from "@react-three/drei";
 import Laptop from "./Components/Laptop";
@@ -95,7 +26,7 @@ export default function App() {
 
       {/* FULLSCREEN WEBSITE */}
       {enterWebsite && (
-        <div className="websiteOverlay" style={{backgroundColor:"yellow"}}>
+        <div className="websiteOverlay" style={{ backgroundColor: "yellow" }}>
           <h1 style={{ backgroundColor: "red" }}>REAL WEBSITE GOES HERE</h1>
         </div>
       )}
