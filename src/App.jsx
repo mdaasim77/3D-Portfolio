1c8a8692cf95ea31f08d4ccc9f9e244aa513b4b8
mdaasim77 <khanaasim9910@gmail.com>
2026-02-28 18:56:17 +0530

    work from home

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Hotspots from "./Components/Hotspots";
 import HotspotInfo from "./Ui/HotspotInfo";
 
 export default function App() {
-  const [start, setStart] = useState(false);
+  // const [start, setStart] = useState(false);
   const [explore, setExplore] = useState(false);
   const [focusLaptop, setFocusLaptop] = useState(false);
   const [enterWebsite, setEnterWebsite] = useState(false);
@@ -78,7 +78,6 @@ export default function App() {
         />
         {/* ======== */}
 
-      
         <Laptop
           explore={explore}
           focusLaptop={focusLaptop}
