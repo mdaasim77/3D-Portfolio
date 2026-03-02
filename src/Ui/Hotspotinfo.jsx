93f55907022f8c08766155771156bde562d27b0e
mdaasim77 <khanaasim9910@gmail.com>
2026-03-02 17:33:21 +0530

    extra code remove

--- src/Ui/Hotspotinfo.jsx
+++ src/Ui/Hotspotinfo.jsx
@@ -1,19 +1,3 @@
-// export default function HotspotInfo({ name }) {
-//   const data = {
-//     Projects: "Here you will showcase your best work.",
-//     Skills: "React, Three.js, GSAP, WebGL, UI/UX.",
-//     About: "Creative front-end developer building immersive experiences.",
-//   };
-
-//   return (
-//     <div className="hotspotCard">
-//       <h2>{name}</h2>
-//       <p>{data[name]}</p>
-//     </div>
-//   );
-// }
-
-
 import { useEffect, useRef } from "react";
 import gsap from "gsap";
 
@@ -24,7 +8,7 @@ export default function HotspotInfo({ name }) {
     gsap.fromTo(
       cardRef.current,
       { y: 100, opacity: 0 },
-      { y: 0, opacity: 1, duration: 0.8, ease: "power3.out" }
+      { y: 0, opacity: 1, duration: 0.8, ease: "power3.out" },
     );
   }, [name]);
 
@@ -33,8 +17,7 @@ export default function HotspotInfo({ name }) {
       "A collection of my best interactive and production-ready projects.",
     Skills:
       "React, Three.js, GSAP, WebGL, UI/UX and performance-focused development.",
-    About:
-      "Creative front-end developer building immersive web experiences.",
+    About: "Creative front-end developer building immersive web experiences.",
   };
 
   return (
